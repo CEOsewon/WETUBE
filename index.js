c9bf0826eeffe40a1358a76b59af6ec3024ad042
CEOsewon <59468796+CEOsewon@users.noreply.github.com>
2020-03-19 18:56:01 +0900

    #2.5 ES6 on NodeJS using Babel

--- index.js
+++ index.js
@@ -1,23 +1,18 @@
-const express = require("express");
+import express from "express";
+
 const app = express();
 
 const PORT = 4000;
 
-function handleListening() {
+// Controller
+const handleListening = () =>
   console.log(`✅ Listening on: http://localhost:${PORT}`);
-}
+const handleHome = (req, res) => res.send("Hello from home");
+const handleProfile = (req, res) => res.send("You are on my profile");
 
-function handleHome(req, res) {
-  console.log(req);
-  res.send("Hello from home");
-}
-
+// Router
 app.get("/", handleHome);
-
-function handleProfile(req, res) {
-  res.send("You are on my profile");
-}
-
 app.get("/profile", handleProfile);
 
+// Initialize
 app.listen(PORT, handleListening);
