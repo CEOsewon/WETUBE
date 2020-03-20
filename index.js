01a10fa2dbe36efcd130bb80463ab082d0b04294
CEOsewon <59468796+CEOsewon@users.noreply.github.com>
2020-03-20 18:09:40 +0900

    #2.6 Express Core: Middlewares

--- index.js
+++ index.js
@@ -4,11 +4,18 @@ const app = express();
 
 const PORT = 4000;
 
-// Controller
+// Function
 const handleListening = () =>
   console.log(`✅ Listening on: http://localhost:${PORT}`);
 const handleHome = (req, res) => res.send("Hello from home");
 const handleProfile = (req, res) => res.send("You are on my profile");
+const betweenHome = (req, res, next) => {
+  console.log("I'm between");
+  next();
+};
+
+// Middleware
+app.use(betweenHome);
 
 // Router
 app.get("/", handleHome);
