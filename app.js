44f8275daf2657614730905dfc6244f89357223d
CEOsewon <59468796+CEOsewon@users.noreply.github.com>
2020-04-09 16:27:54 +0900

    #2.8 Express Core: Routing

--- app.js
+++ app.js
@@ -3,20 +3,13 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
+import { userRouter } from "./router";
 
 const app = express();
 
-const PORT = 4000;
-
 // 함수
-const handleListening = () =>
-  console.log(`✅ Listening on: http://localhost:${PORT}`);
 const handleHome = (req, res) => res.send("Hello from home");
 const handleProfile = (req, res) => res.send("You are on my profile");
-const betweenHome = (req, res, next) => {
-  console.log("I'm between");
-  next();
-};
 
 // 미들웨어
 app.use(helmet()); // 앱 보안
@@ -28,6 +21,6 @@ app.use(morgan("dev")); // HTTP req(요청) 로깅
 // 라우터
 app.get("/", handleHome);
 app.get("/profile", handleProfile);
+app.use("/user", userRouter);
 
-// 서버 작동
-app.listen(PORT, handleListening);
+export default app;
