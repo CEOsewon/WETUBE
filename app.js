2b0c20bca1d8c94781fa4478d56486fccf17084b
CEOsewon <59468796+CEOsewon@users.noreply.github.com>
2020-04-17 18:00:12 +0900

    #2.9 MVC Pattern part One

--- app.js
+++ app.js
@@ -3,13 +3,13 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
-import { userRouter } from "./router";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+import globalRouter from "./routers/globalRouter";
 
 const app = express();
 
 // 함수
-const handleHome = (req, res) => res.send("Hello from home");
-const handleProfile = (req, res) => res.send("You are on my profile");
 
 // 미들웨어
 app.use(helmet()); // 앱 보안
@@ -19,8 +19,8 @@ app.use(cookieParser()); // 사용자정보(로그인정보) 쿠키 저장
 app.use(morgan("dev")); // HTTP req(요청) 로깅
 
 // 라우터
-app.get("/", handleHome);
-app.get("/profile", handleProfile);
+app.use("/", globalRouter);
 app.use("/user", userRouter);
+app.use("/video", videoRouter);
 
 export default app;
