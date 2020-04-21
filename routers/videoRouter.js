f7e3d0e83a3f157513a4707c47f50ec8eaf46f8e
CEOsewon <59468796+CEOsewon@users.noreply.github.com>
2020-04-21 17:19:55 +0900

    #2.11 MVC Pattern part Three

--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -1,12 +1,19 @@
 import express from "express";
 import routes from "../routes";
+import {
+  videos,
+  upload,
+  editVideo,
+  deleteVideo,
+  videoDetail,
+} from "../controllers/videoControllers";
 
 const videoRouter = express.Router();
 
-videoRouter.get(routes.videos, (req, res) => res.send("Videos"));
-videoRouter.get(routes.upload, (req, res) => res.send("Upload"));
-videoRouter.get(routes.editVideo, (req, res) => res.send("Edit video"));
-videoRouter.get(routes.deleteVideo, (req, res) => res.send("Delete Video"));
-videoRouter.get(routes.videoDetail, (req, res) => res.send("Video detail"));
+videoRouter.get(routes.videos, videos);
+videoRouter.get(routes.upload, upload);
+videoRouter.get(routes.editVideo, editVideo);
+videoRouter.get(routes.deleteVideo, deleteVideo);
+videoRouter.get(routes.videoDetail, videoDetail);
 
 export default videoRouter;
