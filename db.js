9a4ce85562916b48696724b9d4496d765102b787
CEOsewon <59468796+CEOsewon@users.noreply.github.com>
2020-05-20 17:17:17 +0900

    #3.1 Connecting to MongoDB

--- db.js
+++ db.js
@@ -1,54 +1,14 @@
-export const videos = [
-  {
-    id: 11111,
-    title: "Number 1",
-    description: "This is Something I love",
-    views: 50,
-    videoFile:
-      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creator: {
-      id: 1234,
-      name: "Sewon",
-      email: "Se@wo.n",
-    },
-  },
-  {
-    id: 22222,
-    title: "Number 2",
-    description: "This is Something I love",
-    views: 50,
-    videoFile:
-      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creator: {
-      id: 1234,
-      name: "Sewon",
-      email: "Se@wo.n",
-    },
-  },
-  {
-    id: 33333,
-    title: "Number 3",
-    description: "This is Something I love",
-    views: 50,
-    videoFile:
-      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creator: {
-      id: 1234,
-      name: "Sewon",
-      email: "Se@wo.n",
-    },
-  },
-  {
-    id: 44444,
-    title: "Number 4",
-    description: "This is Something I love",
-    views: 50,
-    videoFile:
-      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creator: {
-      id: 1234,
-      name: "Sewon",
-      email: "Se@wo.n",
-    },
-  },
-];
+import mongoose from "mongoose";
+
+mongoose.connect("mongodb://localhost:27017/we-tube", {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+});
+
+const db = mongoose.connection;
+
+const handleOpen = () => console.log("✅ Connected to DB");
+const handleError = () => console.log(`❌ Error on DB Connection:${error}`);
+
+db.once("open", handleOpen);
+db.on("error", handleError);
