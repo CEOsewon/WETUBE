c517a85003d3fa5663ba0bfca2df4c515c283580
CEOsewon <59468796+CEOsewon@users.noreply.github.com>
2020-05-26 17:54:12 +0900

    #3.7 Uploading and Creating a Video part Two

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ const app = express();
 // 미들웨어
 app.use(helmet()); // 앱 보안
 app.set("view engine", "pug"); // 뷰 엔진 설정
+app.use("/uploads", express.static("uploads"));
 app.use(bodyParser.json()); // html form 정보 파싱
 app.use(bodyParser.urlencoded({ extended: true })); // html url qeuryString 파싱
 app.use(cookieParser()); // 사용자정보(로그인정보) 쿠키 저장
