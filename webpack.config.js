b706b238d4efd68fb91dcd92f93f589c7cf40503
CEOsewon <59468796+CEOsewon@users.noreply.github.com>
2020-06-04 15:06:26 +0900

    #4.0 Introduction to Webpack

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+// import path from "path"와 동일함
+const path = require("path");
+
+const ENTRY_FILE = path.resolve(__dirname, "assets", "js", "main.js");
+const OUTPUT_DIR = path.join(__dirname, "static");
+
+const config = {
+  entry: ENTRY_FILE,
+  output: {
+    path: OUTPUT_DIR,
+    filename: "[name].[format]",
+  },
+};
+
+module.exports = config;
