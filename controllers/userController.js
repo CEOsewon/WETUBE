1ac493ecd8ebbeb0b5cc957394b1ed65c5f51d75
CEOsewon <59468796+CEOsewon@users.noreply.github.com>
2020-07-14 21:21:31 +0900

    #8.2 Mute Unmute

--- controllers/userController.js
+++ controllers/userController.js
@@ -154,8 +154,13 @@ export const postChangePassword = async (req, res) => {
 
 // My Profile
 
-export const getMe = (req, res) => {
-  res.render("userDetail", { pageTitle: "User Detail", user: req.user });
+export const getMe = async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).populate("videos");
+    res.render("userDetail", { pageTitle: "User Detail", user });
+  } catch (error) {
+    res.redirect(routes.home);
+  }
 };
 
 // User Detail
