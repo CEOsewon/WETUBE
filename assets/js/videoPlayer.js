a11139063560313681f60ec75b76650d9d0ce4c9
CEOsewon <59468796+CEOsewon@users.noreply.github.com>
2020-07-16 08:42:51 +0900

    Revert "#8.3 Enter Fullscreen Exit Fullscreen"
    This reverts commit 1fe1e0cac4bbc5f473032515c18c6d87de0f6000.

--- assets/js/videoPlayer.js
+++ assets/js/videoPlayer.js
@@ -2,7 +2,6 @@ const videoContainer = document.getElementById("jsVideoPlayer");
 const videoPlayer = document.querySelector("#jsVideoPlayer video");
 const playBtn = document.getElementById("jsPlayButton");
 const volumeBtn = document.getElementById("jsVolumeBtn");
-const fullScrnBtn = document.getElementById("jsFullScreen");
 
 function handlePlayClick() {
   if (videoPlayer.paused) {
@@ -24,23 +23,9 @@ function handleVolumeClick() {
   }
 }
 
-function exitFullScreen() {
-  document.exitFullscreen();
-  fullScrnBtn.innerHTML = `<i class="fas fa-expand"></i>`;
-  fullScrnBtn.addEventListener("click", goFullScreen);
-}
-
-function goFullScreen() {
-  videoContainer.requestFullscreen();
-  fullScrnBtn.innerHTML = `<i class="fas fa-compress"></i>`;
-  fullScrnBtn.removeEventListener("click", goFullScreen);
-  fullScrnBtn.addEventListener("click", exitFullScreen);
-}
-
 function init() {
   playBtn.addEventListener("click", handlePlayClick);
   volumeBtn.addEventListener("click", handleVolumeClick);
-  fullScrnBtn.addEventListener("click", goFullScreen);
 }
 
 if (videoContainer) {
