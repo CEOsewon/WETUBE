bfb4ce51f6284577b66ab12aa08290a25e877a77
CEOsewon <59468796+CEOsewon@users.noreply.github.com>
2020-07-31 20:31:46 +0900

    Revert "#10.0 API Registering a View part One"
    This reverts commit 9cbf0fd4cc83a35c15568a4fe272c1d60e2d7d3c.

--- routers/apiRouter.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import routes from "../routes";
-import { getRegisterView } from "../controllers/videoControllers";
-
-const apiRouter = express.Router();
-
-apiRouter.get(routes.registerView, getRegisterView);
-
-export default apiRouter;
