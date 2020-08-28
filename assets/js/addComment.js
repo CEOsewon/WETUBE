0b620d7802243c4a10938f42e0529fea451cb015
CEOsewon <59468796+CEOsewon@users.noreply.github.com>
2020-08-28 17:45:41 +0900

    #Finished add comment part

--- assets/js/addComment.js
+++ assets/js/addComment.js
@@ -1,16 +1,34 @@
 import axios from "axios";
 
 const addCommentForm = document.getElementById("jsAddCommentForm");
+const commentList = document.getElementById("jsCommentList");
+const commentNumber = document.getElementById("jsCommentNumber");
+
+function addCommentNumber() {
+  commentNumber.innerHTML = parseInt(commentNumber.innerHTML, 10) + 1;
+}
+
+function paintComment(comment) {
+  const li = document.createElement("li");
+  const span = document.createElement("span");
+  span.innerHTML = comment;
+  li.appendChild(span);
+  commentList.prepend(li);
+  addCommentNumber();
+}
 
 async function addComment(comment) {
   const videoId = window.location.href.split("videos/")[1];
-  await axios({
+  const response = await axios({
     method: "post",
     url: `/api/${videoId}/comment`,
     data: {
       comment,
     },
   });
+  if (response.status === 200) {
+    paintComment(comment);
+  }
 }
 
 function handleSubmit(event) {
